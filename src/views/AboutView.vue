a1ce7ed34303974d146b48384f96d1b1878a5dad
JonPulat-099 <rjpm023@gmail.com>
2024-06-06 19:17:12 +0500

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,71 @@
+<script setup>
+const cardOptions = [
+  {
+    title: "Бесплатный выпуск",
+    icon: "free.svg",
+  },
+  {
+    title: "До 100 млн. сум",
+    icon: "books.svg",
+  },
+  {
+    title: "55 дней без процентов",
+    icon: "casca.svg",
+  },
+  {
+    title: "Работает в любой точке мира",
+    icon: "idea.svg",
+  },
+];
+</script>
+<template>
+  <div class="view py-9 px-[18px]">
+    <header
+      class="flex items-center justify-between dark:text-white px-5 pt-8 pb-5"
+    >
+      <h2 class="font-bold text-[22px]">Кредитная карта.</h2>
+      <button
+        class="w-[30px] h-[30px] rounded-3xl bg-[#EEF2F6] dark:bg-[#314151] text-xl flex items-center justify-center"
+        @click="$router.push('/')"
+      >
+        <img src="/icons/close.svg" alt="">
+      </button>
+    </header>
+    <article>
+      <p class="px-5 dark:text-white">
+        На сегодняшний день это самая удобная и самая современная карта. Будь в
+        тренде!
+      </p>
+      <iframe
+        src="https://3d-card-view.netlify.app/"
+        frameborder="0"
+        class="my-5 w-full aspect-[3/2]"
+      ></iframe>
+    </article>
+    <article>
+      <div class="mx-auto">
+        <div
+          class="mt-6 grid grid-cols-2 gap-[10px] sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+        >
+          <div
+            v-for="o in cardOptions"
+            class="dark:bg-[#20303F] border-[1px] border-[#E0E4E8] bg-[#F6F8F9] dark:border-none dark:shadow-[0_10px_40px_0_#0E1D28] rounded-[20px] p-5"
+          >
+            <span class="font-bold text-[16px] leading-normal text-[#333] dark:text-white"> {{ o.title }} </span>
+            <div class="flex justify-between items-center mt-6">
+              <span
+                class="w-[30px] h-[30px] bg-white flex items-center justify-center rounded-[30px]"
+              >
+                <img src="/icons/check.svg" alt="" />
+              </span>
+              <img :src="`/icons/${o.icon}`" alt="" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <button type="button" class="w-full bg-[#3D9A73] my-5 p-4 rounded-[10px] text-white">Открыть карту</button>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,72 @@
+<script setup>
+const products = [
+
+
+  {
+    id: 1,
+    name: 'Карта Visa Start',
+    href: '/view',
+    imageSrc: 'https://ru.ipakyulibank.uz/uploads/images/widget/2024/05/widget_1715843113_2633.png.webp',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '0 UZS',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Карта Mastercard Virtual',
+    href: '/view',
+    imageSrc: 'https://ru.ipakyulibank.uz/uploads/images/widget/2022/10/widget_1666789327_1834.png',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '0 UZS',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Карта Visa Start',
+    href: '/view',
+    imageSrc: 'https://ru.ipakyulibank.uz/uploads/images/widget/2024/05/widget_1715843113_2633.png.webp',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '0 UZS',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Карта Mastercard Virtual',
+    href: '/view',
+    imageSrc: 'https://ru.ipakyulibank.uz/uploads/images/widget/2022/10/widget_1666789327_1834.png',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '0 UZS',
+    color: 'Black',
+  },
+  // More products...
+]
+</script>
+
+<template>
+  <main>
+    <div>
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <h2 class="text-2xl font-bold tracking-tight dark:text-white">Банковские платежные карты</h2>
+
+      <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div v-for="product in products" :key="product.id" class="group relative">
+          <div class="flex items-center justify-center p-2  aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img :src="product.imageSrc" :alt="product.imageAlt" class="object-cover object-center" />
+          </div>
+          <div class="mt-4 flex justify-between">
+            <div>
+              <h3 class="text-sm dark:text-white">
+                <RouterLink :to="product.href">
+                  <span aria-hidden="true" class="absolute inset-0" />
+                  {{ product.name }}
+                </RouterLink>
+              </h3>
+            </div>
+            <p class="text-sm font-medium dark:text-[#9EAEBC]">{{ product.price }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+  </main>
+</template>
